a0c86b3a04c07303558a6675910e5c9e4baadcd9
mariana <mariana-medeiros@hotmail.com>
2025-01-17 19:38:08 -0300

    feat: lint

--- scripts/glue_job.py
+++ scripts/glue_job.py
@@ -6,7 +6,9 @@ from awsglue.utils import getResolvedOptions
 from awsglue.job import Job
 from pyspark.sql.functions import col, regexp_replace, upper, when
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME", "DATABASE_NAME", "TABLE_NAME", "TARGET_BUCKET"])
+args = getResolvedOptions(
+    sys.argv, ["JOB_NAME", "DATABASE_NAME", "TABLE_NAME", "TARGET_BUCKET"]
+)
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -19,38 +21,40 @@ TABLE_NAME = args["TABLE_NAME"]
 TARGET_BUCKET = args["TARGET_BUCKET"]
 
 dyframe = glueContext.create_dynamic_frame.from_catalog(
-    database=DATABASE_NAME,
-    table_name=TABLE_NAME,
-    transformation_ctx = "s3files"
+    database=DATABASE_NAME, table_name=TABLE_NAME, transformation_ctx="s3files"
 )
 
 dataframe = dyframe.toDF()
 
 if dataframe.head(1):
 
-    transformed_df = dataframe \
-        .withColumn("order_id",
-                        when(
-                                col("order_id.long").isNotNull(),          
-                                col("order_id.long").cast("string")       
-                            ).otherwise(
-                                regexp_replace(col("order_id.string"), "#", "") 
-                            )) \
-        .withColumn("customer_name", upper(col("customer_name"))) \
-        .withColumn("customer_dob", regexp_replace(col("customer_dob"), "/", "-")) \
-        .withColumn("customer_gender",
-                    when(col("customer_gender") == "F", "FEMALE")
-                    .when(col("customer_gender") == "M", "MALE")
-                    .otherwise(col("customer_gender"))) \
+    transformed_df = (
+        dataframe.withColumn(
+            "order_id",
+            when(
+                col("order_id.long").isNotNull(), col("order_id.long").cast("string")
+            ).otherwise(regexp_replace(col("order_id.string"), "#", "")),
+        )
+        .withColumn("customer_name", upper(col("customer_name")))
+        .withColumn("customer_dob", regexp_replace(col("customer_dob"), "/", "-"))
+        .withColumn(
+            "customer_gender",
+            when(col("customer_gender") == "F", "FEMALE")
+            .when(col("customer_gender") == "M", "MALE")
+            .otherwise(col("customer_gender")),
+        )
         .withColumn("order_date", regexp_replace(col("order_date"), "/", "-"))
+    )
 
-    transformed_dyframe = DynamicFrame.fromDF(transformed_df, glueContext, "transformed_dyframe")
+    transformed_dyframe = DynamicFrame.fromDF(
+        transformed_df, glueContext, "transformed_dyframe"
+    )
 
     glueContext.write_dynamic_frame.from_options(
         frame=transformed_dyframe,
         connection_type="s3",
         connection_options={"path": TARGET_BUCKET},
-        format="parquet"
+        format="parquet",
     )
 
 job.commit()
